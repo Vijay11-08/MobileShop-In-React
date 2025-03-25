254762bd06cd2375766c7059f1869d1e710a598a
JAYSANKALIYA <jaysankaliya2005@gmail.com>
2025-03-25 22:33:09 +0530

    .

--- LikeZone/Like/src/App.jsx
+++ LikeZone/Like/src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import UserNavbar from "../src/components/UserNavbar";
-import Footer from "../src/components/Footer";
-
+import Navbar from './pages/user/navbar';
+import Profile from "./pages/user/Profile";
 const App = () => {
   return (
     <div>
-      <UserNavbar />
+      <Navbar /> {/* Use Navbar instead of UserNavbar */}
       <main className="container">
         {/* Your Page Content Here */}
       </main>
-      {/* Add your Footer Here */}  
       <Footer />
     </div>
   );
