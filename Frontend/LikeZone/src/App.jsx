000342169a9bfc6e6bbcf07952fb5be4a579360f
maharshinimbark <nimbarkmaharshi1108@gmail.com>
2025-04-20 23:41:23 +0530

    Init Project

--- Frontend/LikeZone/src/App.jsx
+++ Frontend/LikeZone/src/App.jsx
@@ -1,115 +1,64 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import NoPage from "./pages/noPage/NoPage";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
+import CartPage from "./pages/cart/CartPage";
+import AllProduct from "./pages/allProduct/AllProduct";
+import Signup from "./pages/registration/Signup";
+import Login from "./pages/registration/Login";
+import UserDashboard from "./pages/user/UserDashboard";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AddProductPage from "./pages/admin/AddProductPage";
+import UpdateProductPage from "./pages/admin/UpdateProductPage";
+import MyState from "./context/myState";
+import { Toaster } from "react-hot-toast";
+import { ProtectedRouteForUser } from "./protectedRoute/ProtectedRouteForUser";
+import { ProtectedRouteForAdmin } from "./protectedRoute/ProtectedRouteForAdmin";
+import CategoryPage from "./pages/category/CategoryPage";
 
-
-import UserHeader from "./components/UserHeader"; // Import Header
-import Home from "./pages/Home";
-import UserDashboard from "./pages/UserDashboard";
-import Checkout from "./pages/Checkout";
-import ProductDetail from "./pages/ProductDetail";
-import Login from "./pages/Login";
-import BuyPage from "./pages/user/buy/BuyPage";
-import SellPage from "./pages/user/sell/SellPage";
-
-
-import SellUsedPhones from "./pages/user/Other/SellUsedPhones";
-import HowItWorks from "./pages/user/Other/HowItWorks";
-import AboutUs from "./pages/user/Other/AboutUs";
-import ContactUs from "./pages/user/Other/ContactUs";
-import FAQs from "./pages/user/Other/FAQs";
-
-import BuyAndroid from "./pages/user/buy/BuyAndroid"; // Import BuyAndroid component
-import BuyIphone from "./pages/user/buy/BuyIphone"; // Import BuyIphone component
-import BuySamsung from "./pages/user/buy/BuySamsung"; // Import BuySamsung component
-import BuyAccessories from "./pages/user/buy/BuyAccessories"; // Import the component
-import BuySmartwatch from "./pages/user/buy/BuySmartwatch"; // Import the component
-import BuyIpad from "./pages/user/buy/BuyIpad"; // Import the component
-import BuyMacbook from "./pages/user/buy/BuyMacbook"; // Import the component
-import Cart from "./pages/user/buy/Cart"; // Import the component
-
-import SelliPhone from "./pages/user/sell/SelliPhone"; // Import the component
-import SellSamsung from "./pages/user/sell/SellSamsung"; // Import the component
-import SelliPad from "./pages/user/sell/SelliPad"; // Import the component
-import SellSmartwatch from "./pages/user/sell/SellSmartwatch"; // Import the component
-import SellAndroidPhone from "./pages/user/sell/SellAndroidPhone"; // Import the component
-import SellAccessories from "./pages/user/sell/SellAccessories"; 
-
-
-import AdminCategories from "./pages/admin/AdminCategories"; // Import component
-import AdminUsers from "./pages/admin/AdminUsers"; // Import component
-import AdminProducts from "./pages/admin/AdminProducts";
-
-
-import Register from "./pages/user/Register"; // Import Register component
-import ResetPassword from "./pages/user/ResetPassword";
-import ForgotPassword from "./pages/user/ForgotPassword";
-
-
-import AdminOrderItems from "./pages/admin/AdminOrderItems";
-import AdminOrders from "./pages/admin/AdminOrders";
-import AdminPayments from "./pages/admin/AdminPayments";
-import Footer from "./components/Footer";
-
-import AddProduct from "./pages/admin/AddProduct";
-
-
-
-
-function App() {
+const App = () => {
   return (
-    <Router>
-      <UserHeader /> {/* Show the header on all pages */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/user" element={<UserDashboard />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/products" element={<ProductDetail />} />
-        <Route path="/login" element={<Login />} />
-
-
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-
-
-        <Route path="/buy/android" element={<BuyAndroid />} />
-        <Route path="/buy/iphone" element={<BuyIphone />} />
-        <Route path="/buy/samsung" element={<BuySamsung />} />
-        <Route path="/buy/accessories" element={<BuyAccessories />} />
-        <Route path="/buy/smartwatch" element={<BuySmartwatch />} />
-        <Route path="/buy/ipad" element={<BuyIpad />} />
-        <Route path="/buy/macbook" element={<BuyMacbook />} />
-        <Route path="/buy/cart" element={<Cart />} />
-
-
-        <Route path="/sell/iphone" element={<SelliPhone />} />
-        <Route path="/sell/samsung" element={<SellSamsung />} />
-        <Route path="/sell/ipad" element={<SelliPad />} />
-        <Route path="/sell/smartwatch" element={<SellSmartwatch />} />
-        <Route path="/sell/android" element={<SellAndroidPhone />} />
-        <Route path="/sell/accessories" element={<SellAccessories />} />
-
-
-        <Route path="/admin/categories" element={<AdminCategories />} />
-        <Route path="/admin/users" element={<AdminUsers />} />
-        <Route path="/admin/products" element={<AdminProducts />} />
-        <Route path="/admin/addproduct" element={<AddProduct />} />
-        <Route path="/admin/addpayments" element={<AdminPayments />} />
-        <Route path="/admin/addorder" element={<AdminOrders />} />
-        <Route path="/admin/addorderitem" element={<AdminOrderItems />} />
-
-
-        <Route path="/Other/faqs" element={<FAQs />} />
-        <Route path="/Other/aboutus" element={<AboutUs />} />
-        <Route path="/Other/contactus" element={<ContactUs />} />
-        <Route path="/Other/howitworks" element={<HowItWorks />} />
-        <Route path="/Other/sellpages" element={<SellUsedPhones />} />
-
-      </Routes>
-
-    <Footer/>
-    </Router>
+    <MyState>
+      <Router>
+        <ScrollTop />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/*" element={<NoPage />} />
+          <Route path="/productinfo/:id" element={<ProductInfo />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/allproduct" element={<AllProduct />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/category/:categoryname" element={<CategoryPage />} />  {/* category Page route  */}
+          <Route path="/user-dashboard" element={
+            <ProtectedRouteForUser>
+              <UserDashboard />
+            </ProtectedRouteForUser>
+          } />
+          <Route path="/admin-dashboard" element={
+            <ProtectedRouteForAdmin>
+              <AdminDashboard />
+            </ProtectedRouteForAdmin>
+          } />
+          <Route path="/addproduct" element={
+            <ProtectedRouteForAdmin>
+              <AddProductPage />
+            </ProtectedRouteForAdmin>
+          } />
+          <Route path="/updateproduct/:id" element={
+            <ProtectedRouteForAdmin>
+              <UpdateProductPage />
+            </ProtectedRouteForAdmin>
+          } />
+        </Routes>
+        <Toaster />
+      </Router>
+    </MyState>
   );
 }
 
--- Frontend/LikeZone/src/main.jsx
+++ Frontend/LikeZone/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { ThemeProvider } from "@material-tailwind/react";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
